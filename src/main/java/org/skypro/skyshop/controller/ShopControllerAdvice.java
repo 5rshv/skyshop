3bb388fd95c8d534529cecd4517669dc7d77550a
5rshv <serishev.alesha@gmail.com>
2025-03-13 21:08:39 +0300

    Commit hw3  spring exception corrected

--- src/main/java/org/skypro/skyshop/controller/ShopControllerAdvice.java
+++ src/main/java/org/skypro/skyshop/controller/ShopControllerAdvice.java
@@ -1,15 +1,18 @@
 package org.skypro.skyshop.controller;
 
+import org.skypro.skyshop.exception.NoSuchProductException;
 import org.skypro.skyshop.exception.ShopError;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestController;
+
 
 @ControllerAdvice
 public class ShopControllerAdvice {
-@ExceptionHandler(IllegalArgumentException.class)
-    public ResponseEntity<ShopError> handleNoSuchProductException(IllegalArgumentException e){
-    return ResponseEntity.badRequest().body(new ShopError("418","i am a teapot"));
-}
+    @ExceptionHandler(NoSuchProductException.class)
+    public ResponseEntity<ShopError> handleNoSuchProductException(NoSuchProductException e){
+        ShopError error = new ShopError("404", "Товар не найден");
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+    }
 }
--- src/main/java/org/skypro/skyshop/exception/NoSuchProductException.java
+++ src/main/java/org/skypro/skyshop/exception/NoSuchProductException.java
@@ -1,6 +1,10 @@
 package org.skypro.skyshop.exception;
 
-public class NoSuchProductException extends java.lang.RuntimeException {
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus (code = HttpStatus.BAD_REQUEST)
+public class NoSuchProductException extends RuntimeException {
     public NoSuchProductException(){
         super("Ничего нет");
     }
